61c7aeab67a74c2422bb3a7542323b36fdfe952a
Dykson <dykson.tech@gmail.com>
2024-06-10 23:48:09 -0300

    Init project, configurations theme application tailwind splashScreen animate

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+    return (
+        <View className="flex-1 justify-center items-center bg-black">
+            <Text className="font-medium text-gray-400">teste</Text>
+        </View>
+    )
+}
